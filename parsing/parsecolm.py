added scaling to parsecolm.py, fixed minor error in quick_plot_python config

parsing/parsecolm.py
@@ -3,11 +3,13 @@
 import sys
 import numpy as np
 
-infile = sys.argv[1]       # input file name
-outfile = sys.argv[2]      # output file name
-popt = sys.argv[3]         # 'a' = (colmR), 'b' = abs(colmR)
-colmL = int(sys.argv[4])   # the column number (start counting at 0) in the data which you would like to be the left column in the output 
-colmR = int(sys.argv[5])   # " " " " " " " " " " " " " " " " " " right " " " "
+infile = sys.argv[1]           # input file name
+outfile = sys.argv[2]          # output file name
+popt = sys.argv[3]             # 'a' = (colmR), 'b' = abs(colmR)
+colmL = int(sys.argv[4])       # the column number (start counting at 0) in the data which you would like to be the left column in the output
+colmR = int(sys.argv[5])       # " " " " " " " " " " " " " " " " " " right " " " "
+scaleL = float(sys.argv[6])    # the scale factor for the left data
+scaleR = float(sys.argv[7])    # " " " " " " right "
 
 
 # read in and parse the data
@@ -17,15 +19,19 @@ with open(infile) as f:
     if '#' in line: continue
     matrix.append(line.split())
 matrix = np.array(matrix)
-A = matrix[:,[colmL,colmR]]
+matrix = matrix[:,[colmL,colmR]]
+A = np.zeros(matrix.shape)
+for i in range(0,A.shape[0]):
+  A[i,0] = float(matrix[i,0])
+  A[i,1] = float(matrix[i,1])
 
 # print data to file
 with open(outfile,'wb') as f:
   for i in range(0,A.shape[0]):
     if popt == 'a':
-      line = str(A[i,0]) + '\t' + str(A[i,1]) + '\n'
+      line = str(scaleL*A[i,0]) + '\t' + str(scaleR*A[i,1]) + '\n'
     elif popt == 'b':
-      line = str(A[i,0]) + '\t' + str(abs(float(A[i,1]))) + '\n'
+      line = str(scaleL*A[i,0]) + '\t' + str(abs(scaleR*A[i,1])) + '\n'
     else:
       print 'popt not recognized!'
       print 'popt = ', popt