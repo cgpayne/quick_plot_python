4a04b1e303056517a6d54ea955182103e54eafb0
charlie_kickflip <c.p0yne@gmail.com>
2019-02-15 14:52:48 +0100

    noted everything out, looks nicer now

--- qpp.py
+++ qpp.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python
+##  head -n 13 qpp.py
+##  python qpp.py config.in output_plot.pdf
+##  By: Charlie Payne, 2018-2019
+## DESCRIPTION
+##  this is basically a proto-wrapper for matplotlib to do a quick plot via python
+##  that said, to make it convenient it's become more of a beast than I had originally intended
+##  and it may very well make a good basis for publishable figures, anyone's welcome to use it!
+##  it should make some sense, especially in combination with reading the default config.in (entry names are moderately intuitive)
+## KNOWN BUGS / DESIRED FEATURES
+##  -- it would be nice to somehow have default values for the 'kscales' and alike, for now rely on default config.in
+## PARAMETERS
+##  1) confile = sys.argv[1]     # the plotting config file, see 'config.in' for the default structure
+##  2) filename = sys.argv[2]    # pdf output file name (including desired extension)
 
 import sys
 import ConfigParser
@@ -8,6 +21,11 @@ confile = sys.argv[1]     # the plotting config file, see 'config.in' for the de
 filename = sys.argv[2]    # pdf output file name (including desired extension)
 
 
+
+# ~~~ function definitions ~~~
+
+# in:   section = section name in the config file, where they're designated by square brackets []
+# out:  dict1 = a python dictionary filled with the config information
 def ConfigSectionMap(section):
   dict1 = {}
   options = Config.options(section)
@@ -21,20 +39,23 @@ def ConfigSectionMap(section):
       dict1[option] = None
   return dict1
 
+# in:   fname = filename, colX/Y = colm in file to use for X/Y-axis
+#         scaleX/Y = scaling factor for X/Y-data, optX/Y = see 'kopts' in config.in
+# out:  nparr = numpy array[N,2] with X- and Y-data based on the above settings
 def ParseFile(fname, colmX, colmY, scaleX, scaleY, optX, optY):
-  tmpmat = []
+  tmpmat = []  # I could probably do this part better, but it works!
   with open(fname) as f:
     for line in f:
-      if '#' in line: continue
+      if '#' in line: continue  # skip commented out lines
       tmpmat.append(line.split())
   tmpmat = np.array(tmpmat)
-  tmpmat = tmpmat[:,[colmX,colmY]]
-  data = np.zeros(tmpmat.shape)
-  for i in range(data.shape[0]):
+  tmpmat = tmpmat[:,[colmX,colmY]]  # strip the data
+  nparr = np.zeros(tmpmat.shape)
+  for i in range(nparr.shape[0]):
     if optX == 'a':
-      data[i,0] = scaleX*float(tmpmat[i,0])
+      nparr[i,0] = scaleX*float(tmpmat[i,0])
     elif optX == 'b':
-      data[i,0] = abs(scaleX*float(tmpmat[i,0]))
+      nparr[i,0] = abs(scaleX*float(tmpmat[i,0]))
     else:
       print 'optX not recognized!'
       print 'fname =',fname
@@ -42,31 +63,22 @@ def ParseFile(fname, colmX, colmY, scaleX, scaleY, optX, optY):
       print 'exiting...'
       exit()
     if optY == 'a':
-      data[i,1] = scaleY*float(tmpmat[i,1])
+      nparr[i,1] = scaleY*float(tmpmat[i,1])
     elif optY == 'b':
-      data[i,1] = abs(scaleY*float(tmpmat[i,1]))
+      nparr[i,1] = abs(scaleY*float(tmpmat[i,1]))
     else:
       print 'optY not recognized!'
       print 'fname =',fname
       print 'optY =',optY
       print 'exiting...'
       exit()
-  return data
-
-#def hmin(array, string):
-#  mins = []
-#  for i in range(len(array)):
-#    mins.append(min(array[i][string]))
-#  return min(mins)
-
-#def hmax(array, string):
-#  maxes = []
-#  for i in range(len(array)):
-#    maxes.append(max(array[i][string]))
-#  return max(maxes)
+  return nparr
 
 
-# set-up the config
+
+# --------- execute the code ---------
+
+# set-up the config, grab the respective dictionaries
 Config = ConfigParser.ConfigParser()
 Config.optionxform=str  # this overrides the default case-insensitivity
 Config.read(confile)
@@ -89,21 +101,21 @@ if figparms['tex'] == 'on':
 
 # parse the files for the data
 knum = int(figparms['knum'])
-data = np.zeros((0,2))
-lens = np.zeros(knum, dtype=np.int)
+data = np.zeros((0,2))  # will hold all the data in one concatenated array, curve by curve...
+lens = np.zeros(knum, dtype=np.int)  # will hold the number of points (lengths) in the data for each curve
 for k in range(knum):
   kX = str(k) + 'X'
   kY = str(k) + 'Y'
   tmpdat = ParseFile(kfiles[str(k)], int(kcolms[kX]), int(kcolms[kY]), float(kscales[kX]), float(kscales[kY]), kopts[kX], kopts[kY])
   lens[k] = np.shape(tmpdat)[0]
-  data = np.concatenate((data,tmpdat))
+  data = np.concatenate((data,tmpdat))  # concatenate data from each curve into one array
 
 # plot the stuff
 fig = figure(figsize=(float(figparms['Xlen']),float(figparms['Ylen'])))
-plotopt = figparms['plot']
-kmin = 0
+plotopt = figparms['plot']  # see 'figparms' in config.in
+kmin = 0  # the lower-bound index for the data points per curve in the concatenated array 'data'
 for k in range(knum):
-  kmax = lens[k] + kmin
+  kmax = lens[k] + kmin  # the upper-bound index " " " " " " " " " " "
   if plotopt == 'a':
     plot(data[kmin:kmax,0], data[kmin:kmax,1], klines[str(k)], color=kcolors[str(k)], label=klabs[str(k)], zorder=int(korders[str(k)]))
   elif plotopt == 'b':
@@ -115,7 +127,7 @@ for k in range(knum):
     print 'figparms[plot] =',plotopt
     print 'exiting...'
     exit()
-  kmin = kmax
+  kmin = kmax  # don't forget to update in the concatenation, see!
 
 # set the limits
 if xylims['xyauto'] == 'on':
@@ -129,14 +141,17 @@ else:
 
 # other formatting
 if figparms['esci'] == 'on':
-  ticklabel_format(style='sci', useMathText=True)  # to put scale from 'e' notation to scientific, if applicable
+  ticklabel_format(style='sci', useMathText=True)  # to put scale from base-e notation to scientific, if applicable
 tick_params(labelsize=float(FS['tickfs']), which='both', direction='in', left=True, bottom=True, top=True, right=True)
 xlabel(figparms['Xlab'], fontsize=float(FS['Xlabfs']))
 ylabel(figparms['Ylab'], fontsize=float(FS['Ylabfs']))
 legend(fontsize=float(FS['legfs']), loc=figparms['legpos'], fancybox=False, edgecolor='black')
 tight_layout()
 
-
+# finalize
 savefig(filename)
 show()
 
+
+
+## FIN
